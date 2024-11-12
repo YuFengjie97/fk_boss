864e900f3164b681582f96e87bb10eb7e5ebfb97
yufengjie <2457522528@qq.com>
2024-11-12 15:44:59 +0800

    ts-node is shit, welcome tsx

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+console.log('1111');
+
+
+// import axios from "axios";
+// import { log } from "console";
+
+// const instance = axios.create({
+//   baseURL: 'https://www.zhipin.com/',
+//   timeout: 1000,
+// })
+
+// instance.get('web/geek/job-recommend').then(res => {
+//   log(res)
+// })
